3c76993c6a984c44a0d0c55052648619f83bb079
muzammilxd001-bit <muzammilxd001@gmail.com>
2026-06-24 12:37:33 +0530

    Add CapSolver support for captcha solving

--- src/captcha.ts
+++ src/captcha.ts
@@ -1,28 +1,48 @@
 import { Constants } from './constants';
 import { CaptchaDataFromRequest } from './interface';
 import { YesCaptchaSolver } from './providers/yescaptcha';
-import { Utils } from './utils';
+import { CapSolverClient } from './providers/capsolver';
 
+let capSolverClient: CapSolverClient | null = null;
 let yesCaptchaClient: YesCaptchaSolver | null = null;
 
-if (process.env.YES_CAPTCHA_API_KEY) {
-	console.log('YesCaptcha API key found. Captcha solving is enabled.');
-	yesCaptchaClient = new YesCaptchaSolver(process.env.YES_CAPTCHA_API_KEY);
+if (process.env.CAPSOLVER_API_KEY) {
+        console.log('CapSolver API key found. Auto captcha solving enabled (CapSolver).');
+        capSolverClient = new CapSolverClient(process.env.CAPSOLVER_API_KEY);
+} else if (process.env.YES_CAPTCHA_API_KEY) {
+        console.log('YesCaptcha API key found. Auto captcha solving enabled (YesCaptcha).');
+        yesCaptchaClient = new YesCaptchaSolver(process.env.YES_CAPTCHA_API_KEY);
+} else {
+        console.warn('No captcha API key set. Reward redemption will fail if Discord requires a captcha.');
+        console.warn('Set CAPSOLVER_API_KEY (recommended) or YES_CAPTCHA_API_KEY to enable auto-solving.');
+}
+
+export function isCaptchaSolvingEnabled(): boolean {
+        return capSolverClient !== null || yesCaptchaClient !== null;
 }
 
 export function solveCaptcha(data: CaptchaDataFromRequest): Promise<string> {
-	// ! its ok
-	// return Utils.askQuestion("Enter the Captcha Key manually: ");
-	// todo: implement captcha solving using 3rd party services like yescaptcha
-	// ! The error rate of Yescaptcha with Discord is extremely high (as already mentioned in the documentation). Do not use it, as it consistently returns error 10008.
-	if (yesCaptchaClient) {
-		return yesCaptchaClient
-			.hcaptcha(data.captcha_sitekey, 'https://discord.com', {
-				rqdata: data.captcha_rqdata,
-				isInvisible: false,
-				userAgent: Constants.USER_AGENT,
-			})
-			.then((result) => result.gRecaptchaResponse);
-	}
-	return Promise.reject(new Error('Captcha solving not implemented yet.'));
+        if (capSolverClient) {
+                return capSolverClient
+                        .hcaptcha(data.captcha_sitekey, 'https://discord.com', {
+                                rqdata: data.captcha_rqdata,
+                                isInvisible: false,
+                                userAgent: Constants.USER_AGENT,
+                        })
+                        .then((result) => result.gRecaptchaResponse);
+        }
+
+        if (yesCaptchaClient) {
+                return yesCaptchaClient
+                        .hcaptcha(data.captcha_sitekey, 'https://discord.com', {
+                                rqdata: data.captcha_rqdata,
+                                isInvisible: false,
+                                userAgent: Constants.USER_AGENT,
+                        })
+                        .then((result) => result.gRecaptchaResponse);
+        }
+
+        return Promise.reject(
+                new Error('No captcha solver configured. Set CAPSOLVER_API_KEY or YES_CAPTCHA_API_KEY.'),
+        );
 }
